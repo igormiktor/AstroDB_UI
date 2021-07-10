d88d7159c314211ac2052d545d3ae7ceadfe78c4
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2021-07-09 21:05:00 -0400

    Create displays for detailed multi-star data.

---
+++ tables/MultipleStarDetails/MultipleStarDetails.php
@@ -0,0 +1,33 @@
+<?php
+
+class tables_MultipleStarDetails
+{
+	function getTitle( &$record )
+	{
+		$title = $record->val( 'name' );
+		if ( $record->val( 'otherNames' ) )
+		{
+			$title .= '; ' . $record->val( 'otherNames' );
+		}
+		return $title;
+	}
+
+	function getDescription( &$record )
+	{
+		$desc = 'Multiple star';
+		$notes = $record->getValue( 'notes' );
+		if ( isset( $notes ) )
+		{
+			$desc .= '; ' . $notes;
+		}
+
+		return $desc;
+	}
+
+	function titleColumn()
+	{
+		return "concat( name, '; ', otherNames )";
+	}
+}
+
+?>
